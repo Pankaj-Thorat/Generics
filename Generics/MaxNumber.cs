c517266a23b8c3e9aca40e0f0477ec3b2866fec9
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-05-15 22:05:45 +0530

    [Pankaj] To find Maximum Number in given numbers

---
+++ Generics/MaxNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class MaxNumber
+    {
+       
+        public static int FindMax(int num1, int num2, int num3)
+        {
+            int max = num1;
+
+            if (num2.CompareTo(max) > 0)
+            {
+                max = num2;
+            }
+
+            if (num3.CompareTo(max) > 0)
+            {
+                max = num3;
+            }
+
+            return max;
+        }
+
+        
+    }
+
+}
+
---
+++ Generics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxNumber maxNumber = new MaxNumber();
+            // Test cases
+            int testCase1 = MaxNumber.FindMax(10, 5, 3);
+            Console.WriteLine("Max number: " + testCase1);  // Expected output: 10
+
+            int testCase2 = MaxNumber.FindMax(7, 15, 8);
+            Console.WriteLine("Max number: " + testCase2);  // Expected output: 15
+
+            int testCase3 = MaxNumber.FindMax(4, 9, 12);
+            Console.WriteLine("Max number: " + testCase3);  // Expected output: 12
+        }
+    }
+}
