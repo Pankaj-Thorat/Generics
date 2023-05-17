674df7801efa5d35fd817c028b4cf5ac6146dacd
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-05-17 17:13:55 +0530

    [Pankaj] to get the Max number in float data type.

---
+++ Generics/FloatMaxNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class FloatMaxNumber
+    {
+        public static float FindMax(float num1, float num2, float num3)
+        {
+            float max = num1;
+
+            if (num2.CompareTo(max) > 0)
+            {
+                max = num2;
+            }
+
+            if (num3.CompareTo(max) > 0)
+            {
+                max = num3;
+            }
+
+            return max;
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -13,13 +13,26 @@ namespace Generics
             MaxNumber maxNumber = new MaxNumber();
             // Test cases
             int testCase1 = MaxNumber.FindMax(10, 5, 3);
-            Console.WriteLine("Max number: " + testCase1);  // Expected output: 10
+            Console.WriteLine("int Max number: " + testCase1);  // Expected output: 10
 
             int testCase2 = MaxNumber.FindMax(7, 15, 8);
-            Console.WriteLine("Max number: " + testCase2);  // Expected output: 15
+            Console.WriteLine("int Max number: " + testCase2);  // Expected output: 15
 
             int testCase3 = MaxNumber.FindMax(4, 9, 12);
-            Console.WriteLine("Max number: " + testCase3);  // Expected output: 12
+            Console.WriteLine("int Max number: " + testCase3);  // Expected output: 12
+
+            //UC 2
+            FloatMaxNumber floatMaxNumber = new FloatMaxNumber();
+            //Test cases
+            float case1 = FloatMaxNumber.FindMax(10.5f, 5.2f, 3.25f);
+            Console.WriteLine("float Max number: " + case1);  // Expected output: 10.5
+
+            float case2 = FloatMaxNumber.FindMax(7.35f, 15.45f, 8.89f);
+            Console.WriteLine("float Max number: " + case2);  // Expected output: 15.45
+
+            float case3 = FloatMaxNumber.FindMax(4.94f, 9.48f, 12.36f);
+            Console.WriteLine("float Max number: " + case3);  // Expected output: 12.36
+
         }
     }
 }
